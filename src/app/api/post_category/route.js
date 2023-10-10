17f9749bcd9b8ec7c2f3c1fe847904d125cdba14
Joy <mmehedihasanjoyv@gmail.com>
2023-10-10 23:51:49 +0600

    complete the task with post_category Item

--- src/app/api/post_category/route.js
+++ src/app/api/post_category/route.js
@@ -1,18 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
-
 const prisma=new PrismaClient()
-export async function POST(req, res) {
- try {
-    await prisma.post_category.create({
-        data: {
-            postId:1,
-            categoryId:2,
-        }
-    })
-     return NextResponse.json({status: 200,message:"create post_category successfully"})
- }
- catch (error) {
-   return NextResponse.json({status:500,message:"failed to create category",error:error,Error_code:error.code})
- }
+export async function POST(req, res, next) { 
+    try {
+        await prisma.post_category.create({
+            data: {
+                postId: 1,
+                categoryId:1
+            }
+        })
+        return NextResponse.json({ status: 200, message:" successful"})
+    } catch (error) {
+        return NextResponse.json({status:500,message:'Failed',error:error.name});
+    }
 }
